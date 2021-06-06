e385b40db44f447c9dcec42b25ac36d3109d6f30
kmorgport <kmorgan0057@gmail.com>
2021-06-06 11:16:05 -0500

    using reactive in composition api

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,40 @@
 <template>
+  <!-- <section class="container">
+    <h2>{{ user.name }}</h2>
+    <h3>{{ user.age }}</h3>
+  </section> -->
   <section class="container">
-    <h2>{{ userName }}</h2>
+    <h2>{{ user2.name }}</h2>
+    <h3>{{ user2.age }}</h3>
   </section>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { reactive } from 'vue';
 export default {
   setup(){
-    const uName = ref('Maximilian');
-
+    // const user = ref({
+    //   name: 'Maximilian',
+    //   age: 31
+    // }),
+    //reactive() only works with objects
+    const otherUser = reactive({
+      name: "Samantha",
+      age: 34
+    })
+    // const uName = ref('Maximilian');
+    // const uAge = ref(31)
     setTimeout(()=>{
-      uName.value = 'Max'
+      // uName.value = 'Max',
+      // uAge.value = 35
+      // user.value.name = 'Max',
+      // user.value.age = 35,
+      otherUser.name = "Sam",
+      otherUser.age = 37
     },2000)
-    return { userName: uName}
+    //to use ref() correctly with an object, pass the object to return and then access object properties
+    //inside the template for it to be reactive
+    return { user2: otherUser}
   }
   // data() {
   //   return {
