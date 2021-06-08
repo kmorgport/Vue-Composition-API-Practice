fb0e7ee3a8052bf7c821f19116a7e2c3b6607d2b
kmorgport <kmorgan0057@gmail.com>
2021-06-08 12:16:55 -0500

    Composition API computed properties and notes

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,18 @@
     <h2>{{ user2.name }}</h2>
     <h3>{{ user2.age }}</h3>
     <button @click="setAge">Change Age</button>
+    <div>
+      <input type="text" placeholder="First Name" @input="setFirstName"/>
+      <input type="text" placeholder="Last Name" @input="setLastName"/>
+    </div>
+  </section>
+  <section>
+    <h2>{{ uName}}</h2>
   </section>
 </template>
 
 <script>
-import { reactive } from 'vue';
+import { ref, reactive, computed } from 'vue';
 export default {
   setup(){
     // const user = ref({
@@ -19,6 +26,8 @@ export default {
     //   age: 31
     // }),
     //reactive() only works with objects
+    const firstName = ref('')
+    const lastName = ref('')
     const otherUser = reactive({
       name: "Samantha",
       age: 34
@@ -27,6 +36,16 @@ export default {
       //dont forget to append .value when changing values of refs
       otherUser.age = 32
     }
+    function setFirstName(event){
+      firstName.value = event.target.value;
+    }
+    function setLastName(event){
+      lastName.value = event.target.value;
+    }
+    //computed refs are read only. not read and write 
+    const uName = computed(()=>{
+      return firstName.value + ' ' + lastName.value
+    })
     // const uName = ref('Maximilian');
     // const uAge = ref(31)
     // setTimeout(()=>{
@@ -39,7 +58,13 @@ export default {
     // },2000)
     //to use ref() correctly with an object, pass the object to return and then access object properties
     //inside the template for it to be reactive
-    return { user2: otherUser, setAge: setNewAge}
+    return { 
+      user2: otherUser,
+       setAge: setNewAge,
+       setFirstName,
+       setLastName,
+       uName
+       }
   }
   // data() {
   //   return {
